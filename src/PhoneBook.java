dd61ffac4f361cda64c261eca011a9aed12e6041
Tymek <tymoteusz@frankiewicz.com.pl>
2018-12-20 18:12:58 +0100

    Add PhoneBoook model class

---
+++ src/PhoneBook.java
@@ -0,0 +1,79 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public class PhoneBook{
+	//klucz to imie, numer to wartosc
+	private ConcurrentMap<String, String> contacts;
+	
+	public static void main(String[] args) {
+		PhoneBook phoneBook = new PhoneBook();
+		phoneBook.load("mapa");
+		System.out.println(phoneBook.list());
+	}
+	
+	public PhoneBook() {
+		contacts = new ConcurrentHashMap<String, String>();
+	}
+
+	@SuppressWarnings("unchecked")
+	public void load(String fileName) {
+		try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(fileName))){
+			contacts =  (ConcurrentMap<String, String>) inputStream.readObject();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void save(String fileName) {
+		try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(fileName))) {
+		    outputStream.writeObject(contacts);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String get(String name) {
+		if(!contacts.containsKey(name)) return "ERROR: Nie ma takiej osoby";
+		else return "OK: " + contacts.get(name);
+	}
+	
+	public String put(String name, String number) {
+		contacts.put(name, number);
+		return "OK";
+	}
+	
+	public String replace(String name, String number) {
+		if(!contacts.containsKey(name)) {
+			return "ERROR: Nie ma takiej osoby";
+		} else {
+			contacts.put(name, number);
+			return "OK";
+		}
+	}
+	
+	public String delete(String name) {
+		contacts.remove(name);
+		return "OK";
+	}
+	
+	public String list() {
+		StringBuilder sb = new StringBuilder();
+		for(String key: contacts.keySet()) {
+			sb.append(key + " ");
+		}
+		return sb.toString();
+	}
+	
+}
--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,29 @@
-public class Server{
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class Server extends JFrame implements ActionListener, Runnable{
+
+	 private static final long serialVersionUID = 1L;
+
+	 static final int SERVER_PORT = 25000;
+
+	 public static void main(String [] args){
+	    new Server();
+	 }
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 	
 }
